4091a989de912832384e82673fd372db9dcc7030
Nikos <nikos.manolas@gmail.com>
2018-12-12 14:53:18 +0000

    changed way rendering worked now it is much better

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import Tetris from '../components/Tetris';
 import { library } from '@fortawesome/fontawesome-svg-core';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
+import _ from 'lodash';
 import './App.css';
 
 library.add(faPlayCircle, faPauseCircle);
@@ -10,10 +11,34 @@ library.add(faPlayCircle, faPauseCircle);
 const numberOfColumns = 10;
 const numberOfRows = 20;
 
-let myBoard = Array(numberOfRows).fill(null).map(row => Array(numberOfColumns).fill(false));
+let piece = {
+  type: '',
+  format: []
+};
 
+piece.type = 'gamma';//needs to be random
+switch (piece.type) {
+  case 'straight':
+    piece.format = [];
+    break;
+  case 'gamma':
+    piece.format = [
+      [true, false],
+      [true, false],
+      [true, true],
+    ];
+    break;
+  default:
+    piece.format = [];
+    break;
+}  
+
+//random piece should start falling
+
+//the activePiece should be spawned randomly in the beginning in a random C position (always begins as r:0)
 const initalState = {
-  tetrisBoard: myBoard  
+  tetrisBoard: Array(numberOfRows).fill(null).map(row => Array(numberOfColumns).fill(false)),
+  activePiece: null
 }
 
 class App extends Component {
@@ -25,60 +50,43 @@ class App extends Component {
   componentDidMount() {}
 
   startGame = (event) => {
-    //random piece should start falling
-    //e.g straight one 4 cells
-    let r = 0;
-    let c = 4;
-    let piece = {
-      type: '',
-      height: 0,
-      width: 0
-    };
-
-    let counter = 0;
-    piece.type = 'gamma';//needs to be random
-    switch (piece.type) {
-      case 'straight':
-        piece.width = 0;
-        piece.height = 4;
-        break;
-      case 'gamma':
-        piece.width = 1;
-        piece.height = 3;
-        break;
-      default:
-        piece.height = 0;
-        piece.width = 0;
-        break;
-    }
-
     const onTimerTick = () => {
-      if (r > 19) {
-        clearInterval(myTimer);
-        return;
-      }
+      // let myBoard = this.buildingBoard();
 
-      if (counter < piece.height) {
-        if (r-piece.height > 0) {
-          myBoard[r-piece.height][c] = false;
-        }
-      } else {
-        myBoard[r-piece.height][c] = false;
-        counter=0;
+      //re-renders after this setState!!!
+      this.setState({activePiece: {
+        r: this.state.activePiece.r + 1,
+        c: this.state.activePiece.c
+      }})
+    }
+    
+    this.setState({
+      activePiece: {
+        format: 'gamma',
+        r: 0,
+        c: 4
       }
-      myBoard[r][c] = true;
-      counter++;      
-      r += 1;
-      this.setState({
-        tetrisBoard: myBoard
-      })      
+    });
+    const myTimer = setInterval(onTimerTick, 500);
+  }
+
+  buildingBoard = () => {
+    const board = _.cloneDeep(this.state.tetrisBoard);
+    if (this.state.activePiece && this.state.activePiece.r + piece.format.length < 21) {
+      //for loop here based on pieces height and width to draw all cells at once.
+      for (let positionX = 0; positionX < piece.format[0].length; positionX++) {
+        for (let positionY = 0; positionY < piece.format.length; positionY++){
+          board[this.state.activePiece.r + positionY][this.state.activePiece.c + positionX] = piece.format[positionY][positionX];
+        }
+      }      
     }
 
-    const myTimer = setInterval(onTimerTick, 500);
-    
+    return board;
   }
 
   render() {
+    const tetrisBoard = this.buildingBoard();
+
     return (
       <div className='App'>
         <nav className = 'main-nav'>
@@ -90,7 +98,7 @@ class App extends Component {
             <li className='push'><a href='./logout'>Logout</a></li>
           </ul>
         </nav>
-        <Tetris myBoard={this.state.tetrisBoard} startGame={this.startGame}></Tetris>
+        <Tetris myBoard={tetrisBoard} startGame={this.startGame}></Tetris>
         <footer>
           {'Tetris game made my Nikos Manolas'}
         </footer>
